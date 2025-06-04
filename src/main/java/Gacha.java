102be11fc348aca6df88a3f8cb205864420e93b1
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-06-04 19:12:00 +0000

    First Ver Gacha Class

---
+++ src/main/java/Gacha.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+public class Gacha{
+
+    private double rates4Star;
+    private double rates5Star;
+    private PlayerCharacter[] fourStarPC;
+    private PlayerCharacter[] fiveStarPC;
+    private Weapon[] fourStarWeapon;
+    private Weapon[] fiveStarWeapon;
+
+    private int cost;
+    private int repeatCompensation;
+
+    public Gacha(PlayerCharacter[] playerCharacterList, Weapon[] weapons){
+    
+        rates4Star = 0.95;
+        rates5Star = 1-rates4Star;
+
+        fourStarPC = playerCharacterList[0];//0 is a temp, its meant to take part of the entire array
+        fiveStarPC = playerCharacterList[0];
+        fourStarWeapon = weapons[0];//0 is a temp, its meant to take part of the entire array
+        fiveStarWeapon = weapons[0];
+
+        cost = 160;
+    }
+
+    public Weapon[] pullWeapon(int timesPulled){
+        Random random = new Random();
+        Weapon[] pulledWeapon = new Weapon[timesToPull];
+        for (int i = 0; i < timesToPull; i++){
+            if(random.nextDouble() <= rates4Star){
+                pulledWeapon[i] = this.fourStarWeapon;
+            }
+            else{
+                pulledWeapon[i] = this.fiveStarWeapon;
+            }
+        }
+        return pulledWeapon;
+    }
+
+        public PlayerCharacter[] pullCharacter(int timesToPull){
+        Random random = new Random();
+        PlayerCharacter[] pulledPC = new PlayerCharacter[timesToPull];
+        for (int i = 0; i < timesToPull; i++){
+            if(random.nextDouble() <= rates4Star){
+                pulledPC[i] = this.fourStarPC;
+            }
+            else{
+                pulledPC[i] = this.fiveStarPC;
+            }
+        }
+        return pulledPC;
+    }
+}
