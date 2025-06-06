5410956f6e531df72945ae721f6afa1869e2b564
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-06-06 03:20:30 +0000

    Create account class with basic stubs

---
+++ src/main/java/Account.java
@@ -0,0 +1,43 @@
+public class Account {
+    private String userName;
+    private String password;
+    private boolean[] playerCharacterUnlocked;
+    // probably should not be a weapon array idk
+    private Weapon[] weaponInv;
+    private int gachaCurrency;
+    private int shopCurrency;
+    private Team team;
+
+    public String displayTeam() {
+        // doesn't need to be done but if this is all the method is it could be removed
+        return getTeam().displayPlayerCharacter();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean[] getPlayerCharacterUnlocked() {
+        return playerCharacterUnlocked;
+    }
+
+    public Weapon[] getWeaponInv() {
+        return weaponInv;
+    }
+
+    public int getGachaCurrency() {
+        return gachaCurrency;
+    }
+
+    public int getShopCurrency() {
+        return shopCurrency;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+}
