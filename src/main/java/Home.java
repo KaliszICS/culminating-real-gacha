66cce1854972efbe600255b2a3265b3ae8a6e770
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-06-08 22:34:57 +0000

    update: 8 new pc added

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,8 +1,16 @@
 public class Home {
     // update manually with every character
     public final PlayerCharacter[] PLAYER_CHARACTER_LIST = new PlayerCharacter[]{
-        
-    };
+    new PlayerCharacter("Diting", "Woof!", 2934, 102, 300, 4, 50, 2, 1, "Damage"), 
+    new PlayerCharacter("Insect Guy", "Mm… It’s Gregor. Well, feel free to talk to me unless I’m asleep.", 3120, 149, 250, 4, 50, 3, 5, "Damage"),
+    new PlayerCharacter("Ika", "Miewwww… Boing", 4673, 122, 530, 4, 100, 2, 3, "Heal"),
+    new PlayerCharacter("Lighting", "HHHHHeeeeeeeeellllllllllllllllllllllllllllllllllllllllooooooooooooo", 6235, 1, 1000, 5, 0, 0, 1, "Damage"),
+    new PlayerCharacter("Anaxa", "Rule number one: Do not call me Anaxa. Rule number two: Never interrupt me — silence is golden. Remember that.", 2800, 97, 756, 5, 75, 2, 5, "Damage"),
+    new PlayerCharacter("Discord", "Make sense? Oh, what fun is there in making sense?", 5341, 92, 329, 5, 50, 2, 5,  "Push", 150),
+    new PlayerCharacter("Rosalina", "May the stars shine down on you...", 3500, 120, 300, 5, 75, 2, 1, "Pull"),
+    new PlayerCharacter("The Ventriloquist", "Its suffering that brings meaning to the futile life", 3450, 100, 250, 5, 35, 3, 3, "Push", 70)
+};
+
     public final Weapon[] WEAPONS = new Weapon[0];
     public final Enemy[] ENEMIES = new Enemy[0];
     private Account account;
