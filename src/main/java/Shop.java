3d63d5de58cd0754f69ec72cefde3c7fa78e9789
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-06-08 20:04:12 +0000

    quick shop update

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -9,25 +9,28 @@ public class Shop {
     //Array of Items (Constant)
     public Item[] ITEMS = new Item[] {
         new Item("Bazooka", 25, "boosts team damage", "Ka-boom"),
-        new Item("Health Elixir", 15, "boosts team helath", "Hallelujah"),
+        new Item("Health Elixir", 15, "boosts team health", "Hallelujah"),
     };
 
-    //ArrayList of items that are on sale; will shoe 3 at one time for the player to choose from, unless they refresh to see 3 different ones
+    //ArrayList of items that are on sale; will show 3 at one time for the player to choose from, unless they refresh to see 3 different ones
     protected ArrayList<Item> itemsOnSale;
     
     /**
      * Constructor
      */
     public Shop() {
-
         this.itemsOnSale = new ArrayList<Item>();
     }
 
     /**
      * 
      */
-    public void refresh() {
+    public void refresh(Account account) {
         
+        if (account.getShopCurrency() >= 15) {
+            
+        }
+
         Random random = new Random();
 
         this.itemsOnSale.clear();
@@ -39,9 +42,9 @@ public class Shop {
     /**
      * 
      * @param item
-     * @param shopCurrency
+     * @param account
      */
-    public void buy(Item item, int shopCurrency) {
+    public void buy(Item item, Account account) {
     }
 
     /**
@@ -49,5 +52,13 @@ public class Shop {
      * @param item
      */
     public void search(Item item) {
+    }
+
+    /**
+     * Getter method
+     * @return the itemsOnSale ArrayList
+     */
+    public ArrayList<Item> getItemsOnSale() {
+        return this.itemsOnSale;
     }
 }
