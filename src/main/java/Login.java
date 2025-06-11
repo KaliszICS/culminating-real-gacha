bca4a43369d6a368b82e96b1449e3ff843c50826
richardg5 <richard.gao5@student.tdsb.on.ca>
2025-06-11 18:42:42 +0000

    Add javadocs for Login

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -3,7 +3,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Utility class representing a login menu for the game.
+ * @version 1.0.0
+ * @author gacha
+ */
 public class Login {
+    /**
+     * Shouldn't be used, private constructor.
+     */
+    private Login() {
+    }
+
+    /**
+     * A login page for the game. Uses user input to get a username and password, checks if an account with that username and password exists, and creates an account object from information in that file.
+     * @return the resulting account object, or null if no account with given username and password exists
+     */
     public static Account login() {
         Account account = null;;
         System.out.print("Please enter your username: ");
@@ -43,6 +58,10 @@ public class Login {
         return account;
     }
 
+    /**
+     * Gets a username and password from userinput, asks user to re-enter their password, and creates an account object with default values if password is re-entered successfully and username or password is not empty.
+     * @return the resulting account object, or null if username or password is empty, or if password is not properly re-entered
+     */
     public static Account signup() {
         Account account = null;
         System.out.print("Please enter your username: ");
@@ -64,6 +83,10 @@ public class Login {
         return account;
     }
 
+    /**
+     * Acts as the main menu of the game. Gets user input for what operation to do, then runs the corresponding method from the Home class for that operation.
+     * @param account the account used for game operations
+     */
     public static void gameStart(Account account) {
         Home home = new Home(account);
         while (home.isPlaying()) {
@@ -119,6 +142,11 @@ public class Login {
         }
     }
 
+    /**
+     * Converts a string of 1s and 0s into a boolean array. 1 represents a true at that index in that boolean array, and 0 represents a false.
+     * @param toConvert the string to be converted
+     * @return the boolean array as a result of the conversion
+     */
     private static boolean[] convertToBoolArr(String toConvert) {
         boolean[] arr = new boolean[toConvert.length()];
         for (int i = 0; i < toConvert.length(); i++) {
